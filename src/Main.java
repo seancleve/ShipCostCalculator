5082c7ad4119a74231b03d9f2c5a6e069eaf4966
seancleve <seanycleve@gmail.com>
2024-09-26 13:22:44 -0400

    shipcost

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+       Scanner in = new Scanner(System.in);
+        double itemPrice;
+        final double FREE_SHIP = 100;
+        double shipCost;
+        double totalCost;
+        final double SHIP_RATE = .02;
+        String trash = "";
+
+        System.out.println("Enter the item price: ");
+        if(in.hasNextDouble()) {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+
+            if(itemPrice >= FREE_SHIP)
+            {
+                shipCost = 0;
+                totalCost = itemPrice;
+            }
+            else
+            {
+                shipCost = SHIP_RATE * itemPrice;
+                totalCost = itemPrice + shipCost;
+
+            }
+            System.out.println("Shipping cost is: " + shipCost);
+            System.out.println("Total cost is: " + totalCost);
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println(trash + " is not a valid number!");
+            System.out.println("Run the program again with correct input!");
+        }
     }
 }
